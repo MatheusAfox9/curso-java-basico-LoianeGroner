c55bd9501a90454d2f0fb5d315b751414950aa91
MatheusAfox9 <112170247+MatheusAfox9@users.noreply.github.com>
2023-05-07 21:11:31 -0300

    Teste

--- curso-java-basico-LoianeGroner/src/com/loiane/cursojava/aula11/VariaveisBoolean.java
+++ curso-java-basico-LoianeGroner/src/com/loiane/cursojava/aula11/VariaveisBoolean.java
@@ -9,7 +9,7 @@ public static void main(String[] args) {
 		boolean falso = false;
 		
 		System.out.println("O valor de verdadeiro é " + verdadeiro);
-		System.out.println("O valor de verdadeiro é " + falso);
+		System.out.println("O valor de verdadeiro é: " + falso);
 		
 
 	}
