bec168a837a63a4d0b9e1d3669fd1642b07901ba
MatheusAfox9 <112170247+MatheusAfox9@users.noreply.github.com>
2023-05-08 22:43:21 -0300

    Quinto Commit

--- curso-java-basico-LoianeGroner/src/com/loiane/cursojava/aula11/VariaveisBoolean.java
+++ curso-java-basico-LoianeGroner/src/com/loiane/cursojava/aula11/VariaveisBoolean.java
@@ -8,8 +8,8 @@ public static void main(String[] args) {
 		
 		boolean falso = false;
 		
-		System.out.println("O valor de verdadeiro é " + verdadeiro);
-		System.out.println("O valor de verdadeiro é " + falso);
+		System.out.println("O valor de verdadeiro é: " + verdadeiro);
+		System.out.println("O valor de verdadeiro é: " + falso);
 		
 
 	}
